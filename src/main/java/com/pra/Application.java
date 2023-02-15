4b81b107adc85d831ba7a332834015b2177ca6ac
Prasad <nrvprasad.aws@gmail.com>
2023-02-14 23:04:30 -0500

    add new variable

--- src/main/java/com/pra/Application.java
+++ src/main/java/com/pra/Application.java
@@ -14,6 +14,7 @@ import com.pra.repository.EmployeeRepository;
 public class Application {
 
 	public static void main(String[] args) {
+		int a= 10;
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
 		EmployeeRepository employeeRepository = context.getBean(EmployeeRepository.class);
